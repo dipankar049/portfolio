5c65e8f19f8e51579db6ff49555faf2f6842831b
Dipankar049 <dipg543@gmail.com>
2025-04-24 01:17:03 +0530

    Fix route paths for GitHub Pages

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,11 +12,11 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 const Home = () => {
   return (
     <>
-      <Router basename="/portfolio/">
+      <Router basename="/portfolio">
         <div className='flex flex-col min-h-screen px-12 bg-gradient-to-br from-gray-50 to-blue-50'>
           <Header />
           <Routes>
-            <Route path='/' element={<Hero />} />
+            <Route path="/portfolio/" element={<Hero />} />
             <Route path='/about' element={<About />} />
             <Route path='/skills' element={<Skills />} />
             <Route path='/projects' element={<Projects />} />
