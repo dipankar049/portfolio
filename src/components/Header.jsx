dff4b4caaa57c35dc5c3288e03a2cdf70201a65f
Dipankar049 <dipg543@gmail.com>
2025-04-24 01:35:46 +0530

    fixed routing

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,16 +8,16 @@ const navLinkClass = ({ isActive }) =>
 export default function Header() {
   return (
     <div className='flex justify-between py-6 sticky top-0 z-[9999] font-bold'>
-      <NavLink to='/portfolio' className="text-xl font-bold">
+      <NavLink to='/' className="text-xl font-bold">
         dipankar049
       </NavLink>
       <div className='flex space-x-6'>
-        <NavLink to='/portfolio' className={navLinkClass}>Home</NavLink>
-        <NavLink to='/portfolio/about' className={navLinkClass}>About</NavLink>
-        <NavLink to='/portfolio/skills' className={navLinkClass}>Skills</NavLink>
-        <NavLink to='/portfolio/projects' className={navLinkClass}>Projects</NavLink>
-        <NavLink to='/portfolio/education' className={navLinkClass}>Education</NavLink>
-        <NavLink to='/portfolio/experience' className={navLinkClass}>Experience</NavLink>
+        <NavLink to='/' className={navLinkClass}>Home</NavLink>
+        <NavLink to='/about' className={navLinkClass}>About</NavLink>
+        <NavLink to='/skills' className={navLinkClass}>Skills</NavLink>
+        <NavLink to='/projects' className={navLinkClass}>Projects</NavLink>
+        <NavLink to='/education' className={navLinkClass}>Education</NavLink>
+        <NavLink to='/experience' className={navLinkClass}>Experience</NavLink>
       </div>
     </div>
   );
