0f877be44dc4cfc973a65897bf324f622605d958
Dipankar049 <dipg543@gmail.com>
2025-04-24 01:57:30 +0530

    Fixed mail link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,7 +12,7 @@ const Footer = () => {
         <a href="https://www.linkedin.com/in/dipankar-karan-9bb755278/" target="_blank" rel="noopener noreferrer">
           <FaLinkedin className="text-2xl hover:text-gray-400" />
         </a>
-        <a href="mailto:depp65@gmail.com" target="_blank" rel="noopener noreferrer">
+        <a href="mailto:dipg543@gmail.com" target="_blank" rel="noopener noreferrer">
           <FaEnvelope className="text-2xl hover:text-gray-400" />
         </a>
       </div>
