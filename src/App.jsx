d561b43e1aa377a3d843a68a46943d261e438ebf
Dipankar049 <dipg543@gmail.com>
2026-01-06 23:15:20 +0530

    style: update background style

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Projects from './pages/Projects';
 import Skills from './pages/Skills';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { ThemeProvider } from './context/ThemeContext';
+import PremiumBackground from './components/PremiumBackground';
 
 function App() {
   return (
@@ -15,25 +16,28 @@ function App() {
       <ThemeProvider>
         <Router>
           {/* <div className='min-h-dvh flex flex-col bg-[linear-gradient(to_bottom_right,_#374151,_#1f2937,_#374151,_#111827)]'> */}
-          <div className='min-h-dvh flex flex-col bg-gradient-to-br from-gray-800 via-gray-700 to-gray-900'>
-            <Header />
-            <div className='my-14 py-4 px-[5vw] grow flex flex-col gap-6'>
-              <Home />
-              <About />
-              <Skills />
-              <Projects />
-              <Experience />
-              <Education />
-              {/* <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/skills" element={<Skills />} />
-                <Route path="/projects" element={<Projects />} />
-                <Route path="/experience" element={<Experience />} />
-                <Route path="/education" element={<Education />} />
-              </Routes> */}
+          <div className="min-h-dvh flex flex-col relative overflow-hidden">
+            <PremiumBackground />
+            <div className="relative z-10">
+              <Header />
+              <div className="my-14 py-4 px-[5vw] grow flex flex-col gap-6">
+                <Home />
+                <About />
+                <Skills />
+                <Projects />
+                <Experience />
+                <Education />
+                {/* <Routes>
+                  <Route path="/" element={<Home />} />
+                  <Route path="/about" element={<About />} />
+                  <Route path="/skills" element={<Skills />} />
+                  <Route path="/projects" element={<Projects />} />
+                  <Route path="/experience" element={<Experience />} />
+                  <Route path="/education" element={<Education />} />
+                </Routes> */}
+              </div>
+              <Footer />
             </div>
-            <Footer />
           </div>
         </Router>
       </ThemeProvider>
